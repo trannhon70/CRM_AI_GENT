01f697986c571169235c6f0cc0b0102bcfeb549c
trannhon70 <kevintran351996@gmail.com>
2026-07-28 16:49:02 +0700

    update

--- src/apis/liveMessage.api.ts
+++ src/apis/liveMessage.api.ts
@@ -6,12 +6,12 @@ export const LiveMessageAPI = {
 };
 
 async function getPaging(query: any) {
-    const respone = await instance.get(`/live-messages/get-paging?pageIndex=${query.pageIndex}&limit=${query.limit}&conversation_id=${query.conversation_id}&search=${query.search}`);
+    const respone = await instance.get(`/chat-service/messages/get-paging?pageIndex=${query.pageIndex}&limit=${query.limit}&conversation_id=${query.conversation_id}&search=${query.search}`);
     return respone.data.data
 }
 
 async function sendMessage(body: any) {
-    const respone = await instance.post(`/live-messages`, body);
+    const respone = await instance.post(`/chat-service/messages`, body);
     return respone.data.data
 }
 
